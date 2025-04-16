aa69bd4bf26b409512bd670f1d93224120618e3c
ASM Tamzid <atkthegreat9999@gmail.com>
2025-04-17 01:05:19 +0600

    added oop object

--- User.js
+++ User.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+//OOP
+class User{
+    constructor(name, age, address){
+        this.name = name;
+        this.age = age;
+        this.location = address;
+    }
+}
+
+
+
 router.get('/users/:id', (req, res)=>{
     const userId = req.params.id;
     const filter = req.query.filter;
@@ -9,12 +20,16 @@ router.get('/users/:id', (req, res)=>{
 });
 
 router.post('/users',(req,res)=>{
-    const {name, age} = req.body;
+    const user = new User(req.body);
+    console.log(req);
 
-    if(!name || !age)
-        return res.status(400).send("Name or age are required");
+    // if(!name || !age)
+    //     return res.status(400).send("Name or age are required");
     
-    res.status(201).json(`User ${name} with age ${age} created`);
+    res.status(201).json({
+        message: "User created successfully",
+        user: user
+    });
 })
 
 module.exports = router;
