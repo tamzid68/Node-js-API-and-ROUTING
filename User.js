bfe5ec839be5961f656d0ab2d3aab41ce550c358
ASM Tamzid <atkthegreat9999@gmail.com>
2025-04-16 22:56:03 +0600

    added post method

--- User.js
+++ User.js
@@ -8,4 +8,13 @@ router.get('/users/:id', (req, res)=>{
     res.send(`User ID: ${userId}, Filter: ${filter}`);
 });
 
+router.post('/users',(req,res)=>{
+    const {name, age} = req.body;
+
+    if(!name || !age)
+        return res.status(400).send("Name or age are required");
+    
+    res.status(201).json(`User ${name} with age ${age} created`);
+})
+
 module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const userRouter = require("./User.js");
 app.use(userRouter);
 
